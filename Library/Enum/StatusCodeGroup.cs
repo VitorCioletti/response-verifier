52e526b248446772fbfb4aad8a3e2e696392cee8
VitorCioletti <vitor.morais@aec.com.br>
2018-08-14 19:33:10 -0300

    Added Enum file

---
+++ Library/Enum/StatusCodeGroup.cs
@@ -0,0 +1,15 @@
+namespace ResponseVerifier
+{
+    public enum StatusCodeGroup
+    {
+        Informational,
+
+        Successful,
+
+        Redirection,
+
+        ClientError,
+    
+        ServerError
+    }
+}
