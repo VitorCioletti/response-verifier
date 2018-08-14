ad1735447d4caef7562924bd0d37ac058be7c705
VitorCioletti <vitor.morais@aec.com.br>
2018-08-14 19:27:45 -0300

    Added unit tests project

---
+++ Tests/ResponseVerifierUnitTests.cs
@@ -0,0 +1,13 @@
+namespace ResponseVerifier.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ResponseVerifierUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
