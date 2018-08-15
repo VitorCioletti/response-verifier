f8121222a30f98ae082fdafd124c999f4b0a0b19
Vitor Cioletti <ciolettiv@gmail.com>
2018-08-15 13:44:55 -0300

    Addded methods summaries

--- Library/ResponseVerifier.cs
+++ Library/ResponseVerifier.cs
@@ -4,18 +4,34 @@ namespace ResponseVerifier
 
     public static class ResponseVerifier
     {
+
+        /// <summary>
+        /// Verify if the current HTTP status is equal/over than 100 and lower than 200
+        /// </summary>
         public static bool IsFromInformationalResponse(this HttpStatusCode statusCode) =>
             IsFrom(StatusCodeGroup.Informational,statusCode);
 
+        /// <summary>
+        /// Verify if the current HTTP status is equal/over than 200 and lower than 300
+        /// </summary>
         public static bool IsFromSucessfulResponse(this HttpStatusCode statusCode) =>
             IsFrom(StatusCodeGroup.Successful, statusCode);
 
+        /// <summary>
+        /// Verify if the current HTTP status is equal/over than 300 and lower than 400
+        /// </summary>
         public static bool IsFromRedirectionResponse(this HttpStatusCode statusCode) =>
             IsFrom(StatusCodeGroup.Redirection, statusCode);
 
+        /// <summary>
+        /// Verify if the current HTTP status is equal/over than 400 and lower than 500
+        /// </summary>
         public static bool IsFromClientErrorResponse(this HttpStatusCode statusCode) =>
             IsFrom(StatusCodeGroup.ClientError,statusCode);
 
+        /// <summary>
+        /// Verify if the current HTTP status is equal/over than 500 and lower than 600
+        /// </summary>
         public static bool IsFromServerErrorResponse(this HttpStatusCode statusCode) =>
             IsFrom(StatusCodeGroup.ServerError, statusCode);
             
